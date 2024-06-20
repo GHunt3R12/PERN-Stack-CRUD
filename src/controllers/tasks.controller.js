5b7495fc956a7ae86cbea35b979d0b7e2456707d
Rafael E <npc@gmail.com>
2024-06-19 19:04:26 -0700

    first commit

---
+++ src/controllers/tasks.controller.js
@@ -0,0 +1,88 @@
+const pool = require("../db");
+
+const getAllTasks = async (req, res) => {
+    try {
+        const allTasks = await pool.query("SELECT * FROM task");
+
+        console.log(allTasks);
+        res.json(allTasks.rows);
+    } catch (error) {
+        next(error);
+    }
+}
+
+const getTask = async(req, res) => {
+    try {
+        const { id } = req.params;
+        const result = await pool.query("SELECT * FROM task WHERE id = $1", [id]);
+    
+        if (result.rows.length === 0) {
+            return res.status(404).json({
+                message: "Task not found.",
+            });
+        }
+    
+        res.json(result.rows[0]);
+    } catch (error) {
+        next(error);
+    }
+}
+
+const createTask = async (req, res) => {
+    const { title, description } = req.body;
+
+    try {
+        const result = await pool.query("INSERT INTO task (title, description) VALUES ($1, $2) RETURNING *", [title, description]);
+
+        res.json(result.rows[0]);
+    } catch (error) {
+        next(error);
+    }
+
+    res.send("Creating a task.");
+}
+
+const deleteTask = async(req, res) => {
+    try {
+        const { id } = req.params;
+        const result = await pool.query("DELETE FROM task WHERE id = $1", [id]);
+    
+        if(result.rowCount === 0) {
+            return res.status(404).json({
+                message: "Task not found.",
+            });
+        }
+    
+        return res.sendStatus(204);
+    } catch (error) {
+        next(error);
+    }
+}
+
+const updateTask = async(req, res) => {
+    try {
+        const { id } = req.params;
+        const { title, description } = req.body;
+    
+        const result = await pool.query("UPDATE task SET title = $1, description = $2 WHERE id = $3 RETURNING *", [title, description, id]);
+    
+        if(result.rows.length === 0) {
+            return res.status(404).json({
+                message: "Task not found.",
+            });
+        }
+    
+        console.log(result);
+        return res.json(result.rows[0]);
+    } catch (error) {
+        next(error);
+    }
+}
+
+module.exports = {
+    getAllTasks,
+    getTask,
+    createTask,
+    deleteTask,
+    updateTask
+}
